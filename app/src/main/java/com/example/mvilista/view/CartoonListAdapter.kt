94f594df617417cd1b8d77d9d45f3950f717c919
Leandro <leo250703@gmail.com>
2022-11-26 12:38:49 -0400

    Commit 26/11/2022 12:38 -> ViewHolder

---
+++ app/src/main/java/com/example/mvilista/view/CartoonListAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.mvilista.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.mvilista.R
+import com.example.mvilista.api.CartoonService
+import com.example.mvilista.model.Cartoon
+
+/*  1) Necesita heredar de la calse adpater
+        pero Adapter de un RecyclerView
+    2) El adaptador necesita obligatoriamente
+        implementar el patron ViewHolder
+    3) La calse interna ViewHolder que creaas
+        debe heredar o extender de la clase
+        abstracta de RecyclerView
+        View Holder
+    4) Los métodos a implemntar refieren
+        mucho al tratamiento con el ViewHolder
+*/
+class CartoonListAdapter(private val cartoons: List<Cartoon>):
+RecyclerView.Adapter<CartoonListAdapter.DataViewHolder>(){
+    //Patrón ViewHolder para el adapter
+    //necesitas la referencia de la View o Layout
+    //que se supone vas a mapear
+    //ncesitas heredar de la clase padre ViewHolder
+    class DataViewHolder(itemView: View):
+    RecyclerView.ViewHolder(itemView) {
+        fun bind(cartoon: Cartoon) {
+            itemView.tvName.text = cartoon.name
+            itemView.tvLocation.text = cartoon.lastLocation
+            val url = CartoonService.BASE_URL + cartoon.image
+            Glide.with(itemView.context)
+                .load(url)
+                .into(itemView.ivCartoon)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup,
+        viewType: Int): DataViewHolder = DataViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.cartoon_item,
+                parent
+            )
+        )
+
+    override fun onBindViewHolder(holder: DataViewHolder,
+        position: Int) {
+        holder.bind(cartoons[position])
+    }
+
+    override fun getItemCount(): Int = cartoons.size
+}
