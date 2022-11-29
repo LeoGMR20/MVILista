7f9b721dea006f40d1d7846eeeaab3eb92fb0d48
Alumno <Alumno@A301-22>
2022-11-29 09:22:53 -0400

    Commit 29/11/2022 9:22(Leandro Morales)

--- app/src/main/java/com/example/mvilista/api/CartoonService.kt
+++ app/src/main/java/com/example/mvilista/api/CartoonService.kt
@@ -2,6 +2,7 @@ package com.example.mvilista.api
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 object CartoonService {
     const val BASE_URL = "https://rickandmortyapi.com/api/"
@@ -10,4 +11,5 @@ object CartoonService {
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+    val api: CartoonApi = getRetrofit().create(CartoonApi::class.java)
 }
---
+++ app/src/main/java/com/example/mvilista/viewmodel/MainViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.mvilista.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvilista.api.CartoonRepo
+import com.example.mvilista.view.MainIntent
+import com.example.mvilista.view.MainState
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.launch
+
+//Para que esta clase se comporte como ViewModel
+//hay que heredar la clase padre de ViewModel
+class MainViewModel(private val repo: CartoonRepo): ViewModel() {
+    //Vamos a trabajar con las abstracciones de Intents y States
+    val userIntent = Channel<MainIntent>(Channel.UNLIMITED)
+    private var _state = MutableStateFlow<MainState>(MainState.Idle)
+    val state : StateFlow<MainState>
+        get() = _state
+
+    init {
+        handleIntent()
+    }
+
+    private fun handleIntent() {
+        viewModelScope.launch {
+            userIntent.consumeAsFlow().collect {
+                when(it) {
+                    //MainIntent.FetchCartoons  = objeto que refleja la intencion de interacciòn
+                    //fetchCartoons() = funcion o metodo que va a interactura con
+                    //en base a los estados
+                    is MainIntent.FetchCartoons -> fetchCartoons()
+                    else -> fetchCartoons()
+                }
+            }
+        }
+    }
+
+    private fun fetchCartoons() {
+        viewModelScope.launch {
+            //Estado cambia de reposo idle a loading
+            _state.value = MainState.Loading
+            _state.value = try {
+                MainState.Cartoons(repo.getCartoons())
+            } catch (e: Exception) {
+                MainState.Error(e.localizedMessage)
+            }
+        }
+    }
+}
